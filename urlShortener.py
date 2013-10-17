44a2347fec5bf8a64bd832a3449514c2030fc3e1
Joyce Wang <joycewang2002@yahoo.com>
2013-10-17 10:09:08 -0400

    completed first part

--- urlShortener.py
+++ urlShortener.py
@@ -1,42 +1,51 @@
 import sys
-import urllib
-import urllib2
 import urlparse
-import requests
 import hashlib
-import string
+import re
 
 def isValidURL(url):
 
     result = urlparse.urlparse(url)
-    if (result.scheme == 'http' or result.scheme == 'https') and result.hostname != None:
-        return result
+
+    # if missing http(s), append to beginning of URL
+    if (result.scheme != 'http' or result.scheme != 'https') and result.hostname == None and result.path != None:
+        url = 'http://' + url
+        result = urlparse.urlparse(url)
+
+    hostname = result.netloc.replace("www.","") # take out 'www.' so it doesn't get hashed
+    match = re.search('.+\..+',hostname) # check if valid URL
+
+    if match:
+        return hostname+result.path+result.params+result.query+result.fragment
     else:
+        print "Error: not a valid URL"
         return -1
 
-def getLongURL(url):
-    return urllib.urlencode({"longurl": url})
 
-def hashURL(netloc):
+def hashURL(validURL):
     hashedURL = hashlib.md5()
-    hashedURL.update(netloc)
+    hashedURL.update(validURL)
     return str(hashedURL.hexdigest()[:5])
 
 def appendDomainName(hashedOutput):
     domain = 'jwang.com/'
     shortenedLink = domain + (str(hashedOutput))
-    print shortenedLink
+    return shortenedLink
 
 def main():
 
-    s = requests.session()
+    if len(sys.argv) != 2:
+        print "Error: invalid input"
+        return
+        
     url = str(sys.argv[1])
     validURL = isValidURL(url)
+
     if validURL == -1:
         return
-    validURL = validURL.netloc.replace("www.","")
+
     hashedOutput = hashURL(validURL)
-    appendDomainName(hashedOutput)
-    print s
+    shortenedURL = appendDomainName(hashedOutput)
+    print shortenedURL
 
 main()
