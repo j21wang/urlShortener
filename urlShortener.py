8aec841e513dce2b1fa02c013dc1ac32863e9226
Joyce Wang <joycewang2002@yahoo.com>
2013-10-18 21:24:26 -0400

    fixed ISSUES

--- urlShortener.py
+++ urlShortener.py
@@ -1,51 +1,49 @@
 import sys
-import urlparse
 import hashlib
 import re
 
-def isValidURL(url):
+def http_check(url):
 
-    result = urlparse.urlparse(url)
-
-    # if missing http(s), append to beginning of URL
-    if (result.scheme != 'http' or result.scheme != 'https') and result.hostname == None and result.path != None:
+    if not url.startswith(('http://','https://')):
         url = 'http://' + url
-        result = urlparse.urlparse(url)
 
-    hostname = result.netloc.replace("www.","") # take out 'www.' so it doesn't get hashed
-    match = re.search('.+\..+',hostname) # check if valid URL
+    return url
+
+def is_valid_url(url):
+
+    match = re.search('.+\..+',url)
 
     if match:
-        return hostname+result.path+result.params+result.query+result.fragment
+        return 1
     else:
-        print "Error: not a valid URL"
         return -1
 
 
-def hashURL(validURL):
-    hashedURL = hashlib.md5()
-    hashedURL.update(validURL)
-    return str(hashedURL.hexdigest()[:5])
+def hash_url(url):
+    hashed_url = hashlib.md5()
+    hashed_url.update(url)
+    return str(hashed_url.hexdigest()[:5])
 
-def appendDomainName(hashedOutput):
-    domain = 'jwang.com/'
-    shortenedLink = domain + (str(hashedOutput))
-    return shortenedLink
+def shorten_url(long_url):
+    long_url = http_check(long_url)
+    is_valid = is_valid_url(long_url)
+
+    if is_valid == -1:
+        return
+    else:
+        hashed_output = hash_url(long_url)
+        shortened_url = 'jwang.com/' + (str(hashed_output))
+        return shortened_url
 
 def main():
-
     if len(sys.argv) != 2:
         print "Error: invalid input"
         return
         
     url = str(sys.argv[1])
-    validURL = isValidURL(url)
-
-    if validURL == -1:
-        return
-
-    hashedOutput = hashURL(validURL)
-    shortenedURL = appendDomainName(hashedOutput)
-    print shortenedURL
-
-main()
+    result = shorten_url(url)
+    print result
+    return
+    
+if __name__ == '__main__':
+    main()
