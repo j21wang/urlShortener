db5f7411824dcf526ccfeda6cce1207425379098
Joyce Wang <joycewang2002@yahoo.com>
2013-10-31 10:26:05 -0400

    encoder and decoder

--- urlShortener.py
+++ urlShortener.py
@@ -1,6 +1,9 @@
 import sys
 import hashlib
 import re
+import requests
+import math
+
 def prepend_http(url):
     if not url.startswith(('http://','https://')):
         url = 'http://' + url
@@ -10,11 +13,33 @@ def prepend_http(url):
 def is_valid_url(url):
     return re.match('https?://.+\..+',url)
 
-def hash_url(url):
-    hashed_url = hashlib.md5()
-    hashed_url.update(url)
-    #return str(hashed_url.hexdigest()[:5])
-    return str(hashed_url.hexdigest())
+def encode(url):
+    chars = "\/\.0123456789abcdefghijklmnopqrstuvwxyz"
+    alnum_url = url.lower()
+    alnum_url = re.sub("^https?://","",alnum_url)
+    
+    short_url = ""
+    for char in alnum_url:
+        remainder_arr = []
+        num = chars.find(char)
+        num = num + 5 # 5 was chosen for funsies
+        short_url = short_url + chars[num]
+
+    return short_url
+
+def decode(url):
+    chars = "\/\.0123456789abcdefghijklmnopqrstuvwxyz"
+    url = re.sub("^.*jwang.com/","",url)
+    url = url.lower()
+    
+    long_url = "" 
+    for char in url:
+        num = chars.find(char)
+        num = num - 5
+        long_url = long_url + chars[num]
+        
+    print long_url
+    return long_url
 
 def shorten_url(long_url):
     long_url = prepend_http(long_url)
@@ -23,24 +48,25 @@ def shorten_url(long_url):
     if is_valid == None:
         raise Exception("Not a valid URL!")
     else:
-        hashed_output = hash_url(long_url)
+        print long_url
+        hashed_output = encode(long_url)
         shortened_url = 'jwang.com/' + (str(hashed_output))
         return shortened_url
 
 def main():
+    s = requests.Session()
     if len(sys.argv) != 2:
         print "Error: invalid input"
         return
         
     url = str(sys.argv[1])
 
-    if re.match('(https?://)?jwang.com',url):
-        print 'check backwards!';
-
-    result = shorten_url(url)
-
-    if result != None:
-        print result
+    if re.match('(https?://)?jwang.com/.+',url):
+        decode(url)
+    else:
+        result = shorten_url(url)
+        if result != None:
+            print result
     
 if __name__ == '__main__':
     main()
